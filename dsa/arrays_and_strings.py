df1ae233f6aafc06d05be878415cfdad57cd26b5
Varun <vasahni@gmail.com>
2025-08-28 17:33:03 -0400

    add examples and tests from additional array and string examples

--- dsa/arrays_and_strings.py
+++ dsa/arrays_and_strings.py
@@ -1,4 +1,7 @@
 def check_palindrome(string: str) -> bool:
+    """
+    Example 1: Given a string s, return true if it is a palindrome, false otherwise.
+    """
     string = string.lower()
     left = 0
     right = len(string) - 1
@@ -8,3 +11,47 @@ def check_palindrome(string: str) -> bool:
         left += 1
         right += -1
     return True
+
+
+def find_sum_sorted_array(sorted_numbers: list, expected_total: int) -> bool:
+    """
+    Example 2: Given a sorted array of unique integers and a target integer,
+    return true if there exists a pair of numbers that sum to target, false
+    otherwise. This problem is similar to Two Sum. (In Two Sum, the input is not sorted).
+    """
+    l = 0
+    r = len(sorted_numbers) - 1
+    while l < r:
+        total = sorted_numbers[l] + sorted_numbers[r]
+        if total == expected_total:
+            return True
+        elif total > expected_total:
+            r -= 1
+        elif total < expected_total:
+            l += 1
+    return False
+
+
+def combine_sorted_arrays(arr1: list[int], arr2: list[int]) -> list[int]:
+    """
+    Example 3: Given two sorted integer arrays arr1 and arr2, return a new array
+    that combines both of them and is also sorted.
+    """
+    i = j = 0
+    combined_array = []
+    while (i < len(arr1)) & (j < len(arr2)):
+        if arr1[i] < arr2[j]:
+            combined_array.append(arr1[i])
+            i += 1
+        else:
+            combined_array.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        combined_array.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        combined_array.append(arr2[j])
+        j += 1
+    return combined_array
--- tests/test_arrays_and_strings.py
+++ tests/test_arrays_and_strings.py
@@ -1,5 +1,5 @@
 import pytest
-from dsa.arrays_and_strings import check_palindrome
+from dsa.arrays_and_strings import check_palindrome, combine_sorted_arrays, find_sum_sorted_array
 
 
 @pytest.mark.parametrize(
@@ -14,3 +14,22 @@ from dsa.arrays_and_strings import check_palindrome
 )
 def test_palindrome(word, expected):
     assert check_palindrome(word) == expected
+
+
+@pytest.mark.parametrize(
+    "sorted_array, expected_total, exists",
+    [([1, 2, 4, 6, 8, 9, 14, 15], 14, True), ([1, 2, 3, 4], 14, False), ([1, 3, 5, 6], 11, True)],
+)
+def test_check_for_sum(sorted_array, expected_total, exists):
+    assert find_sum_sorted_array(sorted_array, expected_total) == exists
+
+
+@pytest.mark.parametrize(
+    "arr1, arr2, combined",
+    [
+        ([1, 3, 5, 7], [2, 4, 6, 8], [1, 2, 3, 4, 5, 6, 7, 8]),
+        ([1, 3, 5], [2, 4, 6, 8, 9, 10], [1, 2, 3, 4, 5, 6, 8, 9, 10]),
+    ],
+)
+def test_combine_sorted_array(arr1, arr2, combined):
+    assert combine_sorted_arrays(arr1, arr2) == combined
