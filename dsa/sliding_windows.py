8acdc63d18121311bbb22db514cb5ae17dc04f82
Varun <vasahni@gmail.com>
2025-08-30 10:32:20 -0400

    add flipping 0's example

--- dsa/sliding_windows.py
+++ dsa/sliding_windows.py
@@ -11,7 +11,6 @@ function fn(arr):
             left++
 
         Do some logic to update the answer
-
 """
 
 
@@ -21,6 +20,26 @@ def subarray_highest_sum(arr: list[int], k: int) -> int:
         curr += arr[right]
         while curr > k:
             curr -= arr[left]
+            left += 1
+        ans = max(ans, right - left + 1)
+    return ans
+
+
+def flip_single_zero(s: str) -> int:
+    """
+    You are given a binary string s (a string containing only "0" and "1").
+    You may choose up to one "0" and flip it to a "1". What is the length of
+    the longest substring achievable that contains only "1"?
+
+    Trick: what is the longest substring that contains at most one 0
+    """
+    left = curr = ans = 0
+    for right in range(len(s)):
+        if s[right] == "0":
+            curr += 1
+        while curr > 1:
+            if s[left] == "0":
+                curr -= 1
             left += 1
         ans = max(ans, right - left + 1)
     return ans
--- tests/test_sliding_windows.py
+++ tests/test_sliding_windows.py
@@ -1,8 +1,24 @@
 import pytest
 
-from dsa.sliding_windows import subarray_highest_sum
+from dsa.sliding_windows import flip_single_zero, subarray_highest_sum
 
 
-@pytest.mark.parametrize("arr, k", [([1, 1, 1, 3], 3)])
-def test_subarray_highest_sum(arr, k):
-    assert subarray_highest_sum(arr, k) == 3
+@pytest.mark.parametrize(
+    "arr, k, ans",
+    [
+        ([1, 1, 1, 3], 3, 3),
+        ([3, 1, 2, 7, 4, 2, 1, 1, 5], 8, 4),
+    ],
+)
+def test_subarray_highest_sum(arr, k, ans):
+    assert subarray_highest_sum(arr, k) == ans
+
+
+@pytest.mark.parametrize(
+    "s, ans",
+    [
+        ("1101100111", 5),
+    ],
+)
+def test_flip_single_zero(s, ans):
+    assert flip_single_zero(s) == ans
