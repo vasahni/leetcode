35bb7069cb08aee81f531022a29012878c1900d1
Varun Sahni <varunsahni@Varuns-MacBook-Pro.local>
2025-09-06 20:52:36 -0400

    add remaining examples arrays and strings

--- dsa/sliding_windows.py
+++ dsa/sliding_windows.py
@@ -11,6 +11,10 @@ function fn(arr):
             left++
 
         Do some logic to update the answer
+
+WHEN TO THINK ABOUT
+When a problem asks you to find subarrays that are valid. E.g, find the longest
+valid subarray.
 """
 
 
@@ -42,4 +46,35 @@ def flip_single_zero(s: str) -> int:
                 curr -= 1
             left += 1
         ans = max(ans, right - left + 1)
+    return ans
+
+
+def subarray_product_less_k(nums: list[int], k: int) -> int:
+    left = ans = 0
+    curr = 1
+    for right in range(len(nums)):
+        curr *= nums[right]
+        while curr >= k:
+            curr //= nums[left]
+            left += 1
+        ans += right - left + 1
+    return ans
+
+
+def max_sum_length_k(arr: list[int], k: int) -> int:
+    """
+    Example 4: Given an integer array nums and an integer k, find the sum of
+    the subarray with the largest sum whose length is k.
+
+    At index i, want to remove k elements back. So adding nums[i], and removing
+    nums[i-k]
+    """
+    curr = ans = 0
+    for i in range(k):
+        curr += arr[i]
+    ans = curr
+    for i in range(k, len(arr)):
+        curr += arr[i]
+        curr -= arr[i - k]
+        ans = max(ans, curr)
     return ans
--- tests/test_sliding_windows.py
+++ tests/test_sliding_windows.py
@@ -1,6 +1,10 @@
 import pytest
-
-from dsa.sliding_windows import flip_single_zero, subarray_highest_sum
+from dsa.sliding_windows import (
+    flip_single_zero,
+    max_sum_length_k,
+    subarray_highest_sum,
+    subarray_product_less_k,
+)
 
 
 @pytest.mark.parametrize(
@@ -22,3 +26,23 @@ def test_subarray_highest_sum(arr, k, ans):
 )
 def test_flip_single_zero(s, ans):
     assert flip_single_zero(s) == ans
+
+
+@pytest.mark.parametrize(
+    "arr, k, ans",
+    [
+        ([10, 5, 2, 6], 100, 8),
+    ],
+)
+def test_subarray_product_less_k(arr, k, ans):
+    assert subarray_product_less_k(arr, k) == ans
+
+
+@pytest.mark.parametrize(
+    "arr, k, ans",
+    [
+        ([3, -1, 4, 12, -8, 5, 6], 4, 18),
+    ],
+)
+def test_max_sum_length_k(arr, k, ans):
+    assert max_sum_length_k(arr, k) == ans
